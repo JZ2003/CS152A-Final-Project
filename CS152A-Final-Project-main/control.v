02283f5b00353273366230f026b774252d6267e3
jz_2003 <zhaojianxing2003@gmail.com>
2023-12-03 03:33:05 -0800

    FSM implementation

--- CS152A-Final-Project-main/control.v
+++ CS152A-Final-Project-main/control.v
@@ -1,6 +1,7 @@
 module control(
     clk,
     RESET,
+    PROCEED,
     CONFIRM,
     num_0_db,
     num_1_db,
@@ -18,6 +19,7 @@ module control(
     dig4
 );
 
+input proceed;
 input clk;
 input RESET;
 input CONFIRM;
@@ -45,186 +47,147 @@ reg [6:0] dig4_reg;
 
 reg [1:0] time_reg;
 
-reg status_confirm;
-
-//wire [3:0] secret;
-
-reg  [3:0] secret_reg;
+reg [3:0] secret_reg;
 
 reg [3:0] guess; 
 
-//always @ (*) begin 
-//    secret_reg <= secret;
-//end
+// reg confirmed;
+// reg proceed;
+
+reg [2:0] numTry;
+reg [2:0] state;
+// Define states
+localparam IDLE = 3'b000;
+localparam CONFIRM_PRESSED = 3'b001;
+localparam PROCEED_PRESSED = 3'b010;
+localparam FAIL = 3'b011;
+localparam SUCCESS = 3'b100;        
 
 
-//random random_generate(.RESET(RESET),.secret(secret));
 
-
-
-//always @(posedge CONFIRM) begin 
-//    if (status_confirm == 0) begin
-//        if (guess != 12) begin 
-//            if (guess == secret_reg) begin
-//                dig1_reg <= 7'b0010010; // 'S'
-//                dig2_reg <= 7'b1000110; //'C'
-//                dig3_reg <= 7'b1111111;
-//                dig4_reg <= 7'b1111111; 
-//            end
-//            else if (guess < secret_reg) begin 
-//                dig1_reg <= 7'b1000111; //'L'
-//                dig2_reg <= 7'b1000000; // 'O'
-//                dig3_reg <= 7'b1111111;
-//                dig4_reg <= 7'b1111111;         
-//            end
-//            else if (guess > secret_reg) begin 
-//                dig1_reg <= 7'b0001001; //'H'
-//                dig2_reg <= 7'b1111001; // 'I'
-//                dig3_reg <= 7'b1111111;
-//                dig4_reg <= 7'b1111111;         
-//            end
-//        end
-//        status_confirm <= 1;
-//    end
-    
-//    else begin
-//        status_confirm <= 0;
-//        dig1_reg <= 7'b1111111;
-//        dig2_reg <= 7'b1111111;
-//        dig3_reg <= 7'b1111111;
-//        dig4_reg <= 7'b1111111; 
-//    end
-//end
-
-
-always @ (posedge clk) begin
+always @ (posedge clk or posedge RESET) begin
     if (RESET) begin
         dig1_reg <= 7'b1111111;
         dig2_reg <= 7'b1111111;
         dig3_reg <= 7'b1111111;
         dig4_reg <= 7'b1111111;
-        status_confirm <= 0;
+        // status_confirm <= 0;
+        state <= IDLE;
+        numTry <= 4;
         secret_reg <= $urandom % 10;
         guess <= 12; //Indicate that no guess has been made yet.
     end
-    
-    if (CONFIRM) begin 
-        status_confirm <= 1;
-//        if (status_confirm == 0) begin 
-//            status_confirm <= 1;
-//        end
-//        else begin 
-//            status_confirm <= 0;
-//        end
-    end
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    if (status_confirm) begin
-        if (guess != 12) begin 
-            if (guess == secret_reg) begin
+    else begin
+        case (state)
+            SUCCESS: 
+                // Show SUCCESS INFO
+                // Don't transition to any other state. Only reset will change FAIL state.        
                 dig1_reg <= 7'b0010010; // 'S'
                 dig2_reg <= 7'b1000110; //'C'
                 dig3_reg <= 7'b1111111;
-                dig4_reg <= 7'b1111111; 
-            end
-            else if (guess < secret_reg) begin 
-                dig1_reg <= 7'b1000111; //'L'
-                dig2_reg <= 7'b1000000; // 'O'
-                dig3_reg <= 7'b1111111;
-                dig4_reg <= 7'b1111111;         
-            end
-            else if (guess > secret_reg) begin 
-                dig1_reg <= 7'b0001001; //'H'
-                dig2_reg <= 7'b1111001; // 'I'
-                dig3_reg <= 7'b1111111;
-                dig4_reg <= 7'b1111111;         
-            end
-        end
-    end
-        
-        
-        
-        
-    end    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    else begin
-        if (status_confirm == 0) begin
-            if (num_0_db == 1'b1) begin
-                dig4_reg <= 7'b1000000;
-                guess <= 0;
-            end
-            else if (num_1_db == 1'b1) begin 
-                dig4_reg <= 7'b1111001;
-                guess <= 1;
-            end 
-            else if (num_2_db == 1'b1) begin 
-                dig4_reg <= 7'b0100100;
-                guess <= 2;
-            end     
-            else if (num_3_db == 1'b1) begin 
-                dig4_reg <= 7'b0110000;
-                guess <= 3;
-            end 
-            else if (num_4_db == 1'b1) begin 
-                dig4_reg <= 7'b0011001;
-                guess <= 4;
-            end     
-            else if (num_5_db == 1'b1) begin 
-                dig4_reg <= 7'b0010010;
-                guess <= 5;
-            end     
-            else if (num_6_db == 1'b1) begin 
-                dig4_reg <= 7'b0000010;
-                guess <= 6;
-            end     
-            else if (num_7_db == 1'b1) begin 
-                dig4_reg <= 7'b1111000;
-                guess <= 7;
-            end 
-            else if (num_8_db == 1'b1) begin 
+                case (numTry)
+                    3:
+                        dig4_reg <= 7'b0110000;
+                    2:
+                        dig4_reg <= 7'b0100100;
+                    1:
+                        dig4_reg <= 7'b1111001;
+                    0:                
+                        dig4_reg <= 7'b1000000;     
+            FAIL:
+                // Show Fail INFO
+                // Don't transition to any other state. Only reset will change FAIL state.
+                dig1_reg <= 7'b0000000;
+                dig2_reg <= 7'b0000000;
+                dig3_reg <= 7'b0000000;
                 dig4_reg <= 7'b0000000;
-                guess <= 8;
-            end     
-            else if (num_9_db == 1'b1) begin 
-                dig4_reg <= 7'b0010000;
-                guess <= 9;
-            end 
-            else begin
-                dig4_reg <= 7'b1111111;
-                guess <= 12;
-            end 
+
+            IDLE:
+                if (numTry == 0) begin
+                    state <= FAIL;
+                end
+                else if(CONFIRM && guess != 12) begin
+                    numTry <= numTry - 1;
+                    state <= CONFIRM_PRESSED;
+                end
     
-        end
-        else begin 
-            dig4_reg <= 7'b1111111;
-            guess <= 12;
-        end
+                //Display and remember the user selected number
+                if (num_0_db == 1'b1) begin
+                    dig4_reg <= 7'b1000000;
+                    guess <= 0;
+                end
+                else if (num_1_db == 1'b1) begin 
+                    dig4_reg <= 7'b1111001;
+                    guess <= 1;
+                end 
+                else if (num_2_db == 1'b1) begin 
+                    dig4_reg <= 7'b0100100;
+                    guess <= 2;
+                end     
+                else if (num_3_db == 1'b1) begin 
+                    dig4_reg <= 7'b0110000;
+                    guess <= 3;
+                end 
+                else if (num_4_db == 1'b1) begin 
+                    dig4_reg <= 7'b0011001;
+                    guess <= 4;
+                end     
+                else if (num_5_db == 1'b1) begin 
+                    dig4_reg <= 7'b0010010;
+                    guess <= 5;
+                end     
+                else if (num_6_db == 1'b1) begin 
+                    dig4_reg <= 7'b0000010;
+                    guess <= 6;
+                end     
+                else if (num_7_db == 1'b1) begin 
+                    dig4_reg <= 7'b1111000;
+                    guess <= 7;
+                end 
+                else if (num_8_db == 1'b1) begin 
+                    dig4_reg <= 7'b0000000;
+                    guess <= 8;
+                end     
+                else if (num_9_db == 1'b1) begin 
+                    dig4_reg <= 7'b0010000;
+                    guess <= 9;
+                end 
+                else begin
+                    dig4_reg <= 7'b1111111;
+                    guess <= 12;
+                end 
+
+            CONFIRM_PRESSED:
+                if(PROCEED) begin
+                    state <= PROCEED_PRESSED;
+                end
+
+                if (guess == secret_reg) begin
+                    state <= SUCCESS;
+
+                end
+                else if (guess < secret_reg) begin 
+                    dig1_reg <= 7'b1000111; //'L'
+                    dig2_reg <= 7'b1000000; // 'O'
+                    dig3_reg <= 7'b1111111;
+                    dig4_reg <= 7'b1111111;         
+                end
+                else if (guess > secret_reg) begin 
+                    dig1_reg <= 7'b0001001; //'H'
+                    dig2_reg <= 7'b1111001; // 'I'
+                    dig3_reg <= 7'b1111111;
+                    dig4_reg <= 7'b1111111;         
+                end
+                 
+            PROCEED_PRESSED:
+                if (!PROCEED) begin
+                    state <= IDLE;
+                end
+        end                
+
+        
     end
-end 
+
 
 assign dig1 = dig1_reg;
 assign dig2 = dig2_reg;
